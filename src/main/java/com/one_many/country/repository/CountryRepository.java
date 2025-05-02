808ca760729562cdadbd80967c9fbe9cce2182bb
웃기진않아 <dugudada01@naver.com>
2025-05-02 16:26:02 +0900

    FEAT: Join Fetch 활용

--- src/main/java/com/one_many/country/repository/CountryRepository.java
+++ src/main/java/com/one_many/country/repository/CountryRepository.java
@@ -4,10 +4,16 @@ package com.one_many.country.repository;
 
 import com.one_many.country.entity.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
 
     Optional<CountryEntity> findByCountry(String country);
+
+    @Query("SELECT co from CountryEntity co " +
+            "JOIN FETCH co.cityEntities ci")
+    List<CountryEntity> findAllFetch();
 }
